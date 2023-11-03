55a4a30976031411efa5e06096db726f5d2a7e29
msahassan <124383788+msahassan@users.noreply.github.com>
2023-11-03 21:59:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+  
+      <h1 className='text-3xl font-bold'>Course Registration</h1>
+      
+    </>
+  )
+}
+
+export default App
