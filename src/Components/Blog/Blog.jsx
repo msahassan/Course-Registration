2c6490d4123c360239f5ce560de184f9a08ca447
msahassan <124383788+msahassan@users.noreply.github.com>
2023-11-04 00:55:24 +0600

    courses all card setup in website

--- src/Components/Blog/Blog.jsx
+++ src/Components/Blog/Blog.jsx
@@ -1,18 +1,22 @@
 import PropTypes from "prop-types";
 
 const Blog = ({ blog }) => {
-  const { course_title, course_img } = blog;
+  const { course_title, course_img, details,price,credit } = blog;
 
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-68 bg-base-100 shadow-xl">
     <figure className="px-5 pt-5">
-      <img src={course_img} alt="" className="rounded-xl" />
+      <img  src={course_img} alt="" className="rounded-xl w-60 h-36" />
     </figure>
-    <div className="card-body items-center text-center">
+    <div className="card-body">
       <h2 className="card-title">{course_title}</h2>
-      <p></p>
+      <p>{details}</p>
+      <div className="flex">
+        <p>Price: {price}</p>
+        <p>Credit: {credit}</p>
+      </div>
       <div className="card-actions">
-        <button className="btn btn-primary">Buy Now</button>
+        <button className="bg-green-300 mt-8 pr-16 pl-16 p-2  rounded-lg font-bold">Buy Now</button>
       </div>
     </div>
   </div>
--- src/Components/Blogs/Blogs.jsx
+++ src/Components/Blogs/Blogs.jsx
@@ -14,7 +14,7 @@ const Blogs = () => {
     <div className="w-2/3">
       <h3 className="text-2xl font-medium ">Blogs:{courses.length}</h3>
 
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-3 gap-4">
         {courses.map((blog) => (
           <Blog key={blog.id} blog={blog}></Blog>
         ))}
